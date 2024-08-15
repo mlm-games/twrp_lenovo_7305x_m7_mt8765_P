faa2c05ed365b563b1c232873ce23ee268639b14
ragebreaker <125530737+mlm-games@users.noreply.github.com>
2024-08-15 09:39:07 +0530

    Update omni_mt8765_P.mk

--- omni_mt8765_P.mk
+++ omni_mt8765_P.mk
@@ -6,14 +6,14 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from mt8765_P device
-$(call inherit-product, device/lenovo/mt8765_P/device.mk)
+# $(call inherit-product, device/lenovo/mt8765_P/device.mk)
 
 PRODUCT_DEVICE := mt8765_P
 PRODUCT_NAME := omni_mt8765_P
